325397cb83b643266dafc4568fce1601bef7c211
dleluc <dleluc@sigal.fr>
2024-04-29 11:24:38 +0200

    add Lit server with Webpack

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { html, css, render } from 'lit';
+import './components/my-element';
+
+const App = () => {
+    return html`
+        <div>
+            <h2>Mon application</h2>
+            <my-element></my-element>
+        </div>
+    `
+}
+
+render(App(), document.querySelector('#root') as HTMLElement)
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+const path = require('path');
+
+module.exports = {
+    entry: './out-tsc/index',
+    mode: 'none',
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        port: 3002
+    },
+    output: {
+        filename: 'main.js',
+        path: path.join(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: 'ts-loader',
+            }
+        ]
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'appLit',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './App': './out-tsc/index.js',
+                './MyElement': './out-tsc/components/my-element.js',
+            },
+            shared: { react: { singleton: true }, 'react-dom': { singleton: true } },
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
+};
